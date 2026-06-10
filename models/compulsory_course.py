e514a1aafde42f66b97f2979cee66b1b29aca547
hnguyen013 <hanhnguyen130526@gmail.com>
2026-06-10 12:10:04 +0700

    fix

--- models/compulsory_course.py
+++ models/compulsory_course.py
@@ -3,3 +3,6 @@ from models.course import Course
 class CompulsoryCourse(Course):
     def __init__(self, course_id, course_name, credits, tuition_fee):
         super().__init__(course_id, course_name, credits, tuition_fee)
+
+    def get_course_type(self):
+        return "Compulsory"
--- models/course.py
+++ models/course.py
@@ -1,6 +1,12 @@
-class Course:
+from abc import ABC, abstractmethod
+
+class Course(ABC):
     def __init__(self, course_id, course_name, credits, tuition_fee):
         self.course_id = course_id
         self.course_name = course_name
         self.credits = credits
         self.tuition_fee = tuition_fee
+
+    @abstractmethod
+    def get_course_type(self):
+        pass
